044002f60c3d68a6c56e30dd3ce4b6a74d5b9305
Anton <antongorobec101@gmail.com>
2023-12-24 01:18:34 +0100

    Refactored code, added grid, added input

--- src/main/java/org/example/demofour/ExecutorController.java
+++ src/main/java/org/example/demofour/ExecutorController.java
@@ -4,7 +4,10 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.KeyCode;
@@ -12,44 +15,23 @@ import javafx.scene.layout.AnchorPane;
 import org.example.demofour.executor.MyExecutor;
 import org.fxmisc.richtext.CodeArea;
 import org.fxmisc.richtext.LineNumberFactory;
-import org.fxmisc.richtext.model.StyleSpans;
-import org.fxmisc.richtext.model.StyleSpansBuilder;
+import org.fxmisc.richtext.StyleClassedTextArea;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
-import java.util.*;
+import java.util.ResourceBundle;
 import java.util.function.IntFunction;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ExecutorController implements Initializable {
-    private static final String[] KEYWORDS = new String[]{
-            "as", "as?", "break", "class", "continue", "do",
-            "else", "false", "for", "fun", "if", "in",
-            "interface", "is", "null", "object", "package",
-            "return", "super", "this", "throw", "true",
-            "try", "typealias", "val", "var", "when", "while"
-    };
-    private static final String KEYWORD_PATTERN = "\\b(" + String.join("|", KEYWORDS) + ")\\b";
-    private static final String PAREN_PATTERN = "[()]";
-    private static final String BRACE_PATTERN = "[{}]";
-    private static final String BRACKET_PATTERN = "[\\[\\]]";
-    private static final String STRING_PATTERN = "\"([^\"\\\\]|\\\\.)*\"";
-    private static final String COMMENT_PATTERN = "//[^\n]*" + "|" + "/\\*(.|\\R)*?\\*/";
-    private static final String SEMICOLON_PATTERN = ";";
-    private static final Pattern PATTERN = Pattern.compile(
-            "(?<KEYWORD>" + KEYWORD_PATTERN + ")"
-                    + "|(?<PAREN>" + PAREN_PATTERN + ")"
-                    + "|(?<BRACE>" + BRACE_PATTERN + ")"
-                    + "|(?<BRACKET>" + BRACKET_PATTERN + ")"
-                    + "|(?<SEMICOLON>" + SEMICOLON_PATTERN + ")"
-                    + "|(?<STRING>" + STRING_PATTERN + ")"
-                    + "|(?<COMMENT>" + COMMENT_PATTERN + ")"
-    );
+    private final MyExecutor executor = new MyExecutor();
     public ListView<Label> output;
     public ProgressIndicator progressIndicator;
+    @FXML
+    private StyleClassedTextArea input;
     @FXML
     private AnchorPane placeForCodeArea;
     private CodeArea codeArea;
@@ -57,62 +39,15 @@ public class ExecutorController implements Initializable {
     @FXML
     private Button runButton;
 
-    public static StyleSpans<Collection<String>> computeHighlighting(String text) {
-        Matcher matcher = PATTERN.matcher(text);
-        int lastKwEnd = 0;
-        StyleSpansBuilder<Collection<String>> spansBuilder = new StyleSpansBuilder<>();
-        while (matcher.find()) {
-            String styleClass = getStyleClass(matcher);
-            spansBuilder.add(Collections.emptyList(), matcher.start() - lastKwEnd);
-            spansBuilder.add(Collections.singleton(styleClass), matcher.end() - matcher.start());
-            lastKwEnd = matcher.end();
-        }
-        spansBuilder.add(Collections.emptyList(), text.length() - lastKwEnd);
-        return spansBuilder.create();
-    }
-
-    private static String getStyleClass(Matcher matcher) {
-        String styleClass =
-                matcher.group("KEYWORD") != null ? "keyword" :
-                        matcher.group("PAREN") != null ? "paren" :
-                                matcher.group("BRACE") != null ? "brace" :
-                                        matcher.group("BRACKET") != null ? "bracket" :
-                                                matcher.group("SEMICOLON") != null ? "semicolon" :
-                                                        matcher.group("STRING") != null ? "string" :
-                                                                matcher.group("COMMENT") != null ? "comment" :
-                                                                        null; /* never happens */
-        assert styleClass != null;
-        return styleClass;
-    }
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        codeArea = new CodeArea();
+        initializeCodeArea();
+        initializeInputField();
+        initializeRunButton();
+        initializeOutputField();
+    }
 
-        IntFunction<Node> numberFactory = LineNumberFactory.get(codeArea);
-
-        codeArea.setParagraphGraphicFactory(numberFactory);
-
-        AnchorPane.setTopAnchor(codeArea, 0d);
-        AnchorPane.setBottomAnchor(codeArea, 0d);
-        AnchorPane.setLeftAnchor(codeArea, 0d);
-        AnchorPane.setRightAnchor(codeArea, 0d);
-
-        placeForCodeArea.getChildren().add(codeArea);
-
-        codeArea.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
-        codeArea.richChanges()
-                .filter(ch -> !ch.getInserted().equals(ch.getRemoved())) // ignore style changes
-                .subscribe(change -> {
-                    codeArea.setStyleSpans(0, computeHighlighting(codeArea.getText()));
-                });
-
-        for (int i = 0; i < 50; i++) {
-            codeArea.appendText("\n");
-        }
-
-        runButton.setOnAction(event -> run());
-
+    private void initializeOutputField() {
         output.setOnKeyPressed(event -> {
             if (event.isControlDown() && event.getCode() == KeyCode.C) {
                 Label selectedLabel = output.getSelectionModel().getSelectedItem();
@@ -126,49 +61,103 @@ public class ExecutorController implements Initializable {
         });
     }
 
+    private void initializeRunButton() {
+        runButton.setOnAction(event -> run());
+    }
+
+    private void initializeInputField() {
+        input.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                String userInput = input.getText();
+                for (String str : userInput.split("\\R")) {
+                    executor.sendInputToProcess(str);
+                }
+                input.clear();
+                event.consume();
+            }
+        });
+    }
+
+    private void initializeCodeArea() {
+        codeArea = new CodeArea();
+        IntFunction<Node> numberFactory = LineNumberFactory.get(codeArea);
+        codeArea.setParagraphGraphicFactory(numberFactory);
+
+        AnchorPane.setTopAnchor(codeArea, 0d);
+        AnchorPane.setBottomAnchor(codeArea, 0d);
+        AnchorPane.setLeftAnchor(codeArea, 0d);
+        AnchorPane.setRightAnchor(codeArea, 0d);
+
+        placeForCodeArea.getChildren().add(codeArea);
+
+        codeArea.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
+        codeArea.richChanges()
+                .filter(ch -> !ch.getInserted().equals(ch.getRemoved())) // ignore style changes
+                .subscribe(change -> {
+                    codeArea.setStyleSpans(0, SyntaxHighlight.computeHighlighting(codeArea.getText()));
+                });
+        for (int i = 0; i < 50; i++) {
+            codeArea.appendText("\n");
+        }
+    }
+
     private void run() {
         sourceCode = codeArea.getText();
         generateKotlin(sourceCode);
-        MyExecutor executor = new MyExecutor();
+
         changeIndicatorState(true);
-        executor.execute(lines -> {
-                    for (String line : lines.split("\n")) {
-                        Label textField = new Label(line);
-                        Platform.runLater(() -> {
-                            textField.setOnMouseClicked(event -> {
-                                Pattern errorMatcher = Pattern.compile(".*\\.kts:(\\d+):\\d+: .*");
-                                Matcher lineMatcher = errorMatcher.matcher(textField.getText());
-                                if (lineMatcher.find()) {
-                                    int lineNumber = Integer.parseInt(lineMatcher.group(1));
-                                    moveToErrorLine(lineNumber);
-                                }
-                                Pattern exceptionPattern = Pattern.compile(".*\\.kts:(\\d+).*");
-                                Matcher exceptionMatcher = exceptionPattern.matcher(line);
-                                System.out.println(line);
-                                if (exceptionMatcher.find()) {
-                                    int lineNumber = Integer.parseInt(exceptionMatcher.group(1));
-                                    moveToErrorLine(lineNumber);
-                                }
-                            });
-                            output.getItems().add(textField);
-                        });
+        runButton.setVisible(false);
+        executor.execute(
+                this::addLineToConsole,
+                this::onProgramStart,
+                this::onProgramEnd);
+    }
 
-                        Pattern errorMatcher = Pattern.compile(".*\\.kts:(\\d+):\\d+: .*");
-                        Matcher lineMatcher = errorMatcher.matcher(textField.getText());
-                        if (lineMatcher.find()) {
-                            textField.setStyle("-fx-text-fill: red;");
-                        }
+    private void addLineToConsole(String lines) {
+        for (String line : lines.split("\n")) {
+            Label textField = new Label(line);
+            Pattern errorPattern = Pattern.compile(".*\\.kts:(\\d+):\\d+: .*");
+            Matcher errorMatcher = errorPattern.matcher(line);
+            Pattern exceptionPattern = Pattern.compile(".*\\.kts:(\\d+).*");
+            Matcher exceptionMatcher = exceptionPattern.matcher(line);
+            Platform.runLater(() -> {
+                textField.setOnMouseClicked((event) -> errorLabelOnClick(line, errorPattern, exceptionPattern));
+                output.getItems().add(textField);
+            });
 
-                        Pattern exceptionPattern = Pattern.compile(".*\\.kts:(\\d+).*");
-                        Matcher exceptionMatcher = exceptionPattern.matcher(line);
-                        System.out.println(line);
-                        if (exceptionMatcher.find()) {
-                            textField.setStyle("-fx-text-fill: red;");
-                        }
-                    }
-                },
-                output,
-                () -> changeIndicatorState(false));
+            if (errorMatcher.find()) {
+                textField.setStyle("-fx-text-fill: red;");
+            }
+
+            if (exceptionMatcher.find()) {
+                textField.setStyle("-fx-text-fill: red;");
+            }
+        }
+    }
+
+    private void errorLabelOnClick(String line, Pattern errorPattern, Pattern exceptionPattern) {
+        Matcher errorMatcher = errorPattern.matcher(line);
+        if (errorMatcher.find()) {
+            int lineNumber = Integer.parseInt(errorMatcher.group(1));
+            moveToErrorLine(lineNumber);
+        }
+
+        Matcher exceptionMatcher = exceptionPattern.matcher(line);
+        if (exceptionMatcher.find()) {
+            int lineNumber = Integer.parseInt(exceptionMatcher.group(1));
+            moveToErrorLine(lineNumber);
+        }
+    }
+
+    private void onProgramStart() {
+        Platform.runLater(() -> output.getItems().clear());
+    }
+
+    private void onProgramEnd() {
+        Platform.runLater(() -> {
+            changeIndicatorState(false);
+            runButton.setVisible(true);
+        });
     }
 
     private void moveToErrorLine(int lineNumber) {
@@ -180,7 +169,6 @@ public class ExecutorController implements Initializable {
 
     private void generateKotlin(String sourceCode) {
         File file = new File("TempKotlin.kts");
-
         try (FileWriter writer = new FileWriter(file)) {
             writer.write(sourceCode);
         } catch (IOException e) {
--- src/main/java/org/example/demofour/HelloApplication.java
+++ src/main/java/org/example/demofour/HelloApplication.java
@@ -3,6 +3,7 @@ package org.example.demofour;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("executor-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+        Scene scene = new Scene(fxmlLoader.load(), Screen.getPrimary().getVisualBounds().getWidth(), Screen.getPrimary().getVisualBounds().getHeight());
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/styles.css")).toExternalForm());
         stage.setTitle("KotlinExecutor");
 
